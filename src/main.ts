c9a254b5d56adbb64a4d1837edb6020c877165b2
Bryson Lindy <bslindy@gmail.com>
2025-10-31 11:08:18 -0700

    removed superfluous npm setup.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
-
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3737;
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
@@ -16,6 +18,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
+  console.log(port);
 }
 bootstrap();
